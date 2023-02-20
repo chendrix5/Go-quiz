18a47877ff8cbb4a1a7b90a55694af847a96e575
clay hendrix <clayhendrix5@gmail.com>
2023-02-20 10:20:36 -0600

    intro done

--- tutorial.go
+++ tutorial.go
@@ -5,7 +5,10 @@ import "fmt"
 func main() {
 	fmt.Println("Welcome to my quiz!")
 
-	name := "Clay"
-	fmt.Printf("Hello %v, how are you doing?", name)
+	fmt.Printf("Enter your name: ")
+	var name string
+	fmt.Scan(&name)
+
+	fmt.Printf("Hello, %v, welcome to the game!", name)
 }
 
